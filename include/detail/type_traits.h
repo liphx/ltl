58bb2bceeccb0b59d22475c0d9e779bcbd34b759
liph <3138519115@qq.com>
2022-09-26 21:06:51 +0800

    allocator

--- include/detail/type_traits.h
+++ include/detail/type_traits.h
@@ -1,6 +1,18 @@
 #ifndef LTL_DETAIL_TYPE_TRAITS_H_
 #define LTL_DETAIL_TYPE_TRAITS_H_
 
+namespace ltl {
+
+template <typename T, T v>
+struct integral_constant {
+    /* static constexpr T value = v; */
+    /* typedef T value_type; */
+    /* typedef integral_constant<T, v> type; */
+    /* constexpr operator value_type() const noexcept { return value; } */
+    /* constexpr value_type operator()() const noexcept { return value; } */
+};
+typedef integral_constant<bool, true> true_type;
+
 template <class T>
 struct remove_reference {
     typedef T type;
@@ -18,5 +30,7 @@ struct remove_reference<T&&> {
 
 template <class T>
 using remove_reference_t = typename remove_reference<T>::type;
+
+}  // namespace ltl
 
 #endif  // LTL_DETAIL_TYPE_TRAITS_H_
