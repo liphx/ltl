0fde58b3b1920ac6f5c2d8348f38a14e5518bb99
liph <3138519115@qq.com>
2022-09-27 21:07:07 +0800

    accumulate, iota, gcd, lcm

--- include/detail/construct.h
+++ include/detail/construct.h
@@ -7,7 +7,7 @@ namespace ltl {
 
 template <class T, class... Args>
 constexpr T *construct_at(T *p, Args&&...args) {
-    return ::new (const_cast<void *>(static_cast<const volatile void *>(p))) T(std::forward<Args>(args)...);
+    return ::new (const_cast<void *>(static_cast<const volatile void *>(p))) T(forward<Args>(args)...);
 }
 
 template <class T>
